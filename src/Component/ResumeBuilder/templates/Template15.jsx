d891c0a12dd4b57c2c8156a71433cd7b09e97e89
Rishi Kashyap <kashyap1strishi@gmail.com>
2024-07-19 17:58:46 +0530

    Update Templates

--- src/Component/ResumeBuilder/templates/Template15.jsx
+++ src/Component/ResumeBuilder/templates/Template15.jsx
@@ -4,17 +4,17 @@ import profilephoto from '../images/profilephoto.png';
 
 const Template15 = ({
     image,
-  data = {},
-  boxBgColor,
-  font,
-  textSize,
-  sectionSpacing,
-  paragraphSpacing,
-  lineSpacing,
-  isPreviewScreen,
-  isTemplate1Previewing,
-  predefinedText = {},
-  skillsfromapi
+    data = {},
+    boxBgColor,
+    font,
+    textSize,
+    sectionSpacing,
+    paragraphSpacing,
+    lineSpacing,
+    isPreviewScreen,
+    isTemplate1Previewing,
+    predefinedText = {},
+    skillsfromapi
 }) => {
   // Define classes based on props
   const textSizeClass = textSize === 'small' ? 'text-sm' : textSize === 'medium' ? 'text-base' : 'text-lg';
@@ -72,45 +72,22 @@ const Template15 = ({
       className={`border-2 border-gray-300 p-7 ${textSizeClass} ${sectionSpacingClass} ${lineHeightClass}`}
       style={{ fontFamily: font }}
     >
-      {/* {!isPreviewScreen && !isTemplate1Previewing && (
-        <div className="">
-          {allDetailsFilled && (
-            <div className="w-7 h-7 ps-2.5 mt-1.5 bg-white rounded-2xl absolute top-48 left-1 font-bold">1</div>
-          )}
-          {allDetailsFilled2 && (
-            <div className="w-7 h-8 ps-2.5 pt-0.5 mt-3.5 bg-white rounded-2xl absolute top-56 left-1 font-bold">2</div>
-          )}
-          {allDetailsFilled3 && (
-            <div className="w-7 h-8 ps-2.5 pt-0.5 mt-11 bg-white rounded-2xl absolute top-60 left-1 font-bold">3</div>
-          )}
-          {allDetailsFilled4 && (
-            <div className="w-7 h-7 ps-2.5 mt-2 bg-white rounded-2xl absolute top-80 left-1 font-bold">4</div>
-          )}
-          {allDetailsFilled5 && (
-            <div className="w-7 h-7 ps-2.5 mt-8 bg-white rounded-2xl absolute top-96 left-1 font-bold">6</div>
-          )}
-          {allDetailsFilled6 && (
-            <div className="w-7 h-8 ps-2.5 pt-1 mt-28 bg-white rounded-2xl absolute top-64 left-1 font-bold">5</div>
-          )}
-        </div>
-      )} */}
-
       <div className="flex items-center">
-      <div className='flex items-center'>
-              <div className="w-24 h-24 bg-white border-4 text-center mr-3 overflow-hidden">
-                <img src={image || profilephoto} alt="Profile" className="w-full h-full object-cover"/>
-              </div>
-            </div>
+        <div className='flex items-center'>
+          <div className="w-24 h-24 bg-white border-4 text-center mr-3 overflow-hidden">
+            <img src={image || profilephoto} alt="Profile" className="w-full h-full object-cover"/>
+          </div>
+        </div>
         <div className="w-3/4">
           {details.map((del, index) => (
             <div key={index}>
-              <h3 className="text-3xl text-blue-700 font-bold">{truncate(del.name || predefinedText.details.name, 20)}</h3>
+              <h3 className="text-3xl text-blue-700 font-bold">{truncate(del.name || predefinedText.details?.name, 20)}</h3>
               <ul className="flex flex-col mt-2 gap-1 text-sm">
-                <li><FaMapMarkerAlt className="inline-block align-text-top mr-1" />{truncate(del.address || predefinedText.details.address, 25)}</li>
-                <li><FaPhoneAlt className="inline-block align-text-top mr-1" />{truncate(del.phoneNumber || predefinedText.details.phoneNumber, 15)}</li>
-                <li><FaEnvelope className="inline-block align-text-top mr-1" />{truncate(del.email || predefinedText.details.email, 25)}</li>
-                <li><FaLinkedin className="inline-block align-text-top mr-1" /><a href={del.link || '#'} target="_blank" rel="noopener noreferrer">{truncate(del.link || predefinedText.details.link, 15)}</a></li>
-                <li><FaGithub className="inline-block align-text-top mr-1" /><a href={del.github || '#'} target="_blank" rel="noopener noreferrer">{truncate(del.github || predefinedText.details.github, 15)}</a></li>
+                <li><FaMapMarkerAlt className="inline-block align-text-top mr-1" />{truncate(del.address || predefinedText.details?.address, 25)}</li>
+                <li><FaPhoneAlt className="inline-block align-text-top mr-1" />{truncate(del.phoneNumber || predefinedText.details?.phoneNumber, 15)}</li>
+                <li><FaEnvelope className="inline-block align-text-top mr-1" />{truncate(del.email || predefinedText.details?.email, 25)}</li>
+                <li><FaLinkedin className="inline-block align-text-top mr-1" /><a href={del.link || '#'} target="_blank" rel="noopener noreferrer">{truncate(del.link || predefinedText.details?.link, 15)}</a></li>
+                <li><FaGithub className="inline-block align-text-top mr-1" /><a href={del.github || '#'} target="_blank" rel="noopener noreferrer">{truncate(del.github || predefinedText.details?.github, 15)}</a></li>
               </ul>
             </div>
           ))}
@@ -121,7 +98,7 @@ const Template15 = ({
         <h5 className="text-blue-700 font-bold">SUMMARY</h5>
         <div className="border-t border-gray-300 mt-1 mb-2"></div>
         {summary.map((sum, index) => (
-          <p key={index} className="text-sm mt-2">{sum.summarydescription || predefinedText.summary.summarydescription}</p>
+          <p key={index} className="text-sm mt-2" dangerouslySetInnerHTML={{ __html: sum.summarydescription || predefinedText.summary?.summarydescription || 'Your Summary' }}></p>
         ))}
       </div>
 
@@ -132,12 +109,12 @@ const Template15 = ({
           <div key={index} className="mt-2">
             <div className="flex justify-between">
               <div>
-                <h6 className="font-bold">{exp.Company || predefinedText.experiences.company}</h6>
-                <p className="text-sm">{exp.role || predefinedText.experiences.role}</p>
+                <h6 className="font-bold">{exp.Company || predefinedText.experiences?.company}</h6>
+                <p className="text-sm">{exp.role || predefinedText.experiences?.role}</p>
               </div>
               <p className="text-sm">{exp.month1} - {exp.month2}</p>
             </div>
-            <p className="text-sm mt-1">{exp.companydescription || predefinedText.experiences.companydescription}</p>
+            <p className="text-sm mt-1" dangerouslySetInnerHTML={{ __html: exp.companydescription || predefinedText.experiences?.companydescription || 'Description of job experience' }}></p>
           </div>
         ))}
       </div>
@@ -149,8 +126,8 @@ const Template15 = ({
           <div key={index} className="mt-2">
             <div className="flex justify-between">
               <div>
-                <h6 className="font-bold">{edu.schoolname || predefinedText.educations.schoolname}</h6>
-                <p className="text-sm">{edu.coursename || predefinedText.educations.coursename}</p>
+                <h6 className="font-bold">{edu.schoolname || predefinedText.educations?.schoolname}</h6>
+                <p className="text-sm">{edu.coursename || predefinedText.educations?.coursename}</p>
               </div>
               <p className="text-sm">{edu.edmonth1} - {edu.edmonth2}</p>
             </div>
@@ -163,7 +140,7 @@ const Template15 = ({
         <div className="border-t border-gray-300 mt-1 mb-2"></div>
         <ul className="text-sm  pl-4">
           {skills.map((skill, index) => (
-            <li key={index}>{skill.skillname || predefinedText.skills.skillname}</li>
+            <li key={index}>{skill.skillname || predefinedText.skills?.skillname}</li>
           ))}
         </ul>
       </div>
@@ -173,8 +150,8 @@ const Template15 = ({
         <div className="border-t border-gray-300 mt-1 mb-2"></div>
         {sectionadd.map((section, index) => (
           <div key={index} className="mt-2">
-            <h6 className="font-bold">{section.sectiontitle || predefinedText.sectionadd.sectiontitle}</h6>
-            <p className="text-sm mt-1">{section.sectiondescription || predefinedText.sectionadd.sectiondescription}</p>
+            <h6 className="font-bold">{section.sectiontitle || predefinedText.sectionadd?.sectiontitle}</h6>
+            <p className="text-sm mt-1">{section.sectiondescription || predefinedText.sectionadd?.sectiondescription}</p>
           </div>
         ))}
       </div>
